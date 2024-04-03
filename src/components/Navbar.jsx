6c0258fe18c49d7a137b0371c72e1daee1faa59b
Tengis <tengisdalai801@gmail.com>
2024-04-03 20:09:49 +0800

    navbar footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+
+export default function Navbar(){
+    return(
+
+        <nav className=" sticky top:0 w-full z-20 top-0 start-0 ">
+          <div className="  max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+              {/* <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"> */}
+              <Image
+                src="https://flowbite.com/docs/images/logo.svg"
+                width={50}
+                height={50}
+                alt="Picture of the author"
+                />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </a>
+          <div className=" flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ">
+              <button type="button" className="text-[#ffffff] border-4 border-[#FFBD59] bg-[#FFBD59] hover:text-[#FFBD59] hover:bg-[#ffffff] focus:ring-4 focus:outline-none focus:ring-[#FFBD59] font-medium rounded-full text-sm px-4 py-2 mx-4 text-center ">Нэвтрэх</button>
+              <button type="button" className=" text-[#ffffff] border-4 border-[#FFBD59] bg-[#FFBD59] hover:text-[#FFBD59] hover:bg-[#ffffff] focus:ring-4 focus:outline-none focus:ring-[#FFBD59] font-medium rounded-full text-sm px-4 py-2 mx-4 text-center ">Бүртгүүлэх</button>
+              <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+          </div>
+          <div className=" items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul className=" flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+              <li>
+                <a href="#" className="block py-[20px] px-3 text-white bg-[#FFBD59] rounded md:bg-transparent md:text-[#FFBD59] md:p-0 " aria-current="page">Нүүр хуудас</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-[#373737] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#FFBD59] md:p-0">Хичээл</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-[#373737] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#FFBD59] md:p-0">Тэтгэлэг</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-[#373737] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#FFBD59] md:p-0">Чиглэл тест</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-[#373737] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#FFBD59] md:p-0">Мэргэжил</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+        );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Nuurhuudas from "@/components/Nuurhuudas";
+import Putter from "@/components/Putter";
+export default function Home1() {
+  return <div>
+   <div>
+    <div><Navbar/>
+    </div>
+    <div><Nuurhuudas/></div>
+    <div><Putter/></div>
+    
 
-export default function Home() {
-  return <div><p>{"paaa"}</p></div>
+   </div>
+  </div>
 }
