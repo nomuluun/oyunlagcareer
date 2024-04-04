52313744337ba6ff28b52dedc2621ea0b48d85f7
ANUJIN <Anujinbattumur11@gmail.com>
2024-04-04 16:53:18 +0800

    signin signup hiiv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import CareerTest from "@/components/CareerTest";
 import Navbar from "@/components/Navbar"
+import Signup from "@/components/Signup"
+import Signin from "@/components/Signin"
+import Job from "@/components/Job"
 export default function Home() {
   return <div>
     {/* <Navbar/> */}
-    <CareerTest/>
+    {/* <CareerTest/> */}
+    {/* <Signin/>  */}
+    <Job/>
     </div>
   return <div><p>{"paaa"}</p></div>
 }
