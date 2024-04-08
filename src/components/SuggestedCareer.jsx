1403c85e2e4cfeba5220a507fa8fddc5d3b63c24
Tengis <tengisdalai801@gmail.com>
2024-04-08 15:08:46 +0800

    uurchluh

--- src/components/SuggestedCareer.jsx
+++ src/components/SuggestedCareer.jsx
@@ -13,19 +13,19 @@ export default function SuggestedCareer({ data }) {
     router.push("/job");
   };
   return (
-    <div className="lg:w-[30%] bg-[#ffffff] text-[#5E17EB] hover:bg-[#5E17EB] hover:text-[#ffffff] rounded-[48px] flex flex-col justify-center items-center shadow-[0_0px_120px_0px_rgba(94,23,235,0.09)] h-[247px] space-y-[20px] w-full max-w-[]">
-      <button onClick={handleSub}>
-        <h1 className="text-[24px] font-bold pt-5">
+    <div className="lg:w-[30%]  grid bg-[#ffffff] md:w-[40vw] text-[#373737] hover:bg-[#5E17EB] hover:text-[#ffffff] rounded-[48px] justify-evenly items-center shadow-[0_0px_120px_0px_rgba(94,23,235,0.09)] h-[250px] space-y-[20px] w-[60vw] max-w-[800px]">
+      <button className="w-full" onClick={handleSub}>
+        <h1 className="text-[22px] font-bold pt-5">
           {" "}
           {data?.jobTitle.charAt(0).toUpperCase() + data?.jobTitle.slice(1)}{" "}
         </h1>
-        <div className="flex flex-row justify-around pt-5">
+        <div className="flex flex-row pt-4">
           <img src={data?.img} alt="" className="w-[90px]" />
-          <p className="w-[182px] ">{ab.substr(0, 50)}...</p>
+          <p className=" pl-5 w-[190px] ">{ab.substr(0, 50)}...</p>
         </div>
-        <div className="flex space-x-5 py-5 justify-around">
-          <p className="font-bold font ">ЭЕШ өгөх хичээлүүд:</p>
-          <div className="flex -space-x-[20px]">
+        <div className="flex space-x-1 py-5 justify-evenly">
+          <p className="font-bold font">ЭЕШ өгөх хичээлүүд:</p>
+          <div className="flex -space-x-[15px]">
             <img src="/svg/bio.png" alt="" className="w-[38px]" />
             <img src="/svg/chem.png" alt="" className="w-[38px]" />
             <img src="/svg/math.png" alt="" className="w-[38px]" />
