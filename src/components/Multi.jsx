2f17fe53ba531933074541f1525f67b4939c9aab
Nomuluun <nomukaolnoo223@gmail.com>
2024-04-08 14:38:02 +0800

    multi

--- src/components/Multi.jsx
+++ src/components/Multi.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // import { MultiSelect } from "react-multi-select-component";
 import { MultiSelect } from 'primereact/multiselect';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { jobList } from "../../utils/data";
 const Multi = () => {
   const options = [
     { name: "Математик", code: "mat" },
@@ -15,15 +16,25 @@ const Multi = () => {
     { name: "Англи хэл", code: "eng" },
     { name: "Монгол хэл", code: "mon" },
   ];
-  // const [selected, setSelected] = useState([]);
-  const [selectedCities, setSelectedCities] = useState(null);
-  const cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' }
-  ];
+const [selectedCities, setSelectedCities] = useState(null);  
+console.log(selectedCities);
+const filt=()=>{
+  // let res = [];
+  
+  const selectJob = jobList.filter((data)=>{
+    const arr = data.lesson;
+    console.log('data',data.lesson[0])
+    // console.log('data',arr[0],arr[1],arr[2]);
+    // data.lesson[0] == 'mat' ? true : false;
+  // console.log(arr.includes('mat'));
+    // if(data.lesson.includes('mat')){
+    //   return true;
+    // }
+    // else {
+    //   return false;
+    // }
+  })
+}
   return (
     <div className=" col-span-2 my-2">
           <h1 className=" font font-extrabold text-[30px] text-[#373737]">
@@ -34,6 +45,7 @@ const Multi = () => {
             <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={options} optionLabel="name" display="chip" 
                 placeholder="Сонгох" maxSelectedLabels={10} className="border-solid w-full justify-center" />
       </div>
+      <button onClick={filt}>haih</button>
     </div>
   );
 };
