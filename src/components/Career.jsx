4b3ba6e24866941f412c7374a31335e9d3c429dc
Nomuluun <nomukaolnoo223@gmail.com>
2024-04-08 17:47:43 +0800

    mergejil duusgav

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,19 +1,30 @@
 import Multi from "@/components/Multi";
+import { useState } from "react";
+import { jobList } from "../../utils/data";
+import SuggestedCareer from "@/components/SuggestedCareer";
+
 export default function Career() {
+  const [selectJob, setSelectJob] = useState(jobList);
   return (
-    <div className="bg-[#F5F4F9] max-w-screen-xl gap-x-3 gap-y-10 mx-auto px-5 md:pt-[40px] pt-[30px] h-auto">
-     <div className="flex">
-        <div className=" grid md:grid-cols-3 md:pt-[10px]">
-          <Multi />
-      <img
-        src="Saly-10.png"
-        className="relative col-span-1 hidden md:block w-full max-w-[500px] "
-        alt=""
-        />
-         <img src="gurvljin.png" className="absolute hidden md:block  top-[80px] left-[350px] w-[20vw] max-w-[200px] md:top-[250px] md:left-[45vw] rotate-12 md: " alt="" />
+    <div>
+      <div className="bg-[#F5F4F9] max-w-screen-xl gap-x-3 gap-y-10 mx-auto px-5 md:pt-[40px] pt-[30px] h-auto ">
+        <div >
+          {/* <div className=" grid md:grid-cols-3 md:pt-[10px] border border-[black]"> */}
+            <Multi setSelectJob={setSelectJob}/>
+            {/* <img
+              src="Saly-10.png"
+              className="relative col-span-1 hidden md:block w-full max-w-[500px] "
+              alt=""
+            />
+            <img
+              src="gurvljin.png"
+              className="absolute hidden md:block  top-[80px] left-[350px] w-[20vw] max-w-[200px] md:top-[250px] md:left-[45vw] rotate-12 md: "
+              alt=""
+            /> */}
+          {/* </div> */}
         </div>
-        </div>
-        <h1 className="font-bold pt-5 font">Хайсан илэрцүүд:</h1>
+    
+      </div>
     </div>
   );
 }
--- src/components/Multi.jsx
+++ src/components/Multi.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 // import { MultiSelect } from "react-multi-select-component";
-import { MultiSelect } from 'primereact/multiselect';
+import { MultiSelect } from "primereact/multiselect";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { jobList } from "../../utils/data";
+import SuggestedCareer from "./SuggestedCareer";
 const Multi = () => {
   const options = [
     { name: "Математик", code: "mat" },
@@ -16,36 +17,60 @@ const Multi = () => {
     { name: "Англи хэл", code: "eng" },
     { name: "Монгол хэл", code: "mon" },
   ];
-const [selectedCities, setSelectedCities] = useState(null);  
-console.log(selectedCities);
-const filt=()=>{
-  // let res = [];
-  
-  const selectJob = jobList.filter((data)=>{
-    const arr = data.lesson;
-    console.log('data',data.lesson[0])
-    // console.log('data',arr[0],arr[1],arr[2]);
-    // data.lesson[0] == 'mat' ? true : false;
-  // console.log(arr.includes('mat'));
-    // if(data.lesson.includes('mat')){
-    //   return true;
-    // }
-    // else {
-    //   return false;
-    // }
-  })
-}
+  const [selectedCities, setSelectedCities] = useState(null);
+  // console.log(selectedCities);
+  const selectJob = jobList.filter((data) => {
+    for (let i = 0; i < selectedCities?.length; i++) {
+      if (!data.lesson.includes(selectedCities[i].code)) {
+        return false;
+      }
+    }
+    return true;
+  });
   return (
-    <div className=" col-span-2 my-2">
+    <div className=" col-span-2 my-2 ">
+      <div className="flex flex-row justify-evenly">
+        <div>
           <h1 className=" font font-extrabold text-[30px] text-[#373737]">
-          Хичээлтэй холбоотой мэргэжлүүд
-        </h1>
-      <p className="text-[20px] font pb-7 mt-5 text-[#373737] max-w-[550px] " >Хичээлүүд сонгон, мэргэжлүүдийн талаарх мэдээллийг авна уу.</p>
-      <div className="items-center justify-center w-full lg:w-[30vw]">
-            <MultiSelect value={selectedCities} onChange={(e) => setSelectedCities(e.value)} options={options} optionLabel="name" display="chip" 
-                placeholder="Сонгох" maxSelectedLabels={10} className="border-solid w-full justify-center" />
+            Хичээлтэй холбоотой мэргэжлүүд
+          </h1>
+          <p className="text-[20px] font pb-7 mt-5 text-[#373737] max-w-[550px] ">
+            Хичээлүүд сонгон, мэргэжлүүдийн талаарх мэдээллийг авна уу.
+          </p>
+          <div className="items-center justify-center w-full lg:w-[30vw]">
+            <MultiSelect
+              value={selectedCities}
+              onChange={(e) => setSelectedCities(e.value)}
+              options={options}
+              optionLabel="name"
+              display="chip"
+              placeholder="Сонгох"
+              maxSelectedLabels={10}
+              className="border-solid w-full justify-center"
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src="Saly-10.png"
+            className="relative col-span-1 hidden md:block w-full max-w-[500px] "
+            alt=""
+          />
+          <img
+            src="gurvljin.png"
+            className="absolute hidden md:block  top-[80px] left-[350px] w-[20vw] max-w-[200px] md:top-[250px] md:left-[45vw] rotate-12 md: "
+            alt=""
+          />  
+        </div>
       </div>
-      <button onClick={filt}>haih</button>
+      <div className=" w-full">
+        <h1 className="font-bold pt-5 font">Хайсан илэрцүүд:</h1>
+        <div className="flex md:flex-row justify-evenly mt-[30px] flex-wrap flex-col p-5 gap-9 items-center max-w-screen-xl mx-auto ">
+          {selectJob.map((data, index) => {
+            return <SuggestedCareer data={data} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
